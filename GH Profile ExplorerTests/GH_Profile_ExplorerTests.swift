8098da9db5f42ae96e47daa9c101c77f5f751566
Josep Escobar <josepescobarn@gmail.com>
2025-05-14 10:58:05 +0200

    Initial Commit

---
+++ GH Profile ExplorerTests/GH_Profile_ExplorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  GH_Profile_ExplorerTests.swift
+//  GH Profile ExplorerTests
+//
+//  Created by Josep Escobar on 14/5/25.
+//
+
+import Testing
+
+struct GH_Profile_ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
